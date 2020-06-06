925926b67192ad55aa852c46a73d582739b51dd3
Lucas Santana <lucasdeosantana@gmail.com>
2020-06-06 00:22:52 -0300

    add midleway to images upload

--- Server/src/routes.ts
+++ Server/src/routes.ts
@@ -1,13 +1,18 @@
 import express, { request } from 'express'
 import pointControler from './controller/pointsController'
 import itemsControler from './controller/itemsController'
+import multerConfig from './config/multer'
+import multer from 'multer'
+
 
 const router = express.Router()
 const point = new pointControler()
-const items =  new itemsControler
+const items =  new itemsControler()
+const upload = multer(multerConfig) 
+
 router.get('/items', items.index)
 router.get('/points/:id', point.show)
 router.get('/points', point.index)
-router.post('/points', point.create)
+router.post('/points', upload.single('image') ,point.create)
 
 export default router
