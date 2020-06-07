26ac88276d26f88937b8dbb21421e665cb8a5f74
Lucas Santana <lucasdeosantana@gmail.com>
2020-06-07 18:46:18 -0300

    page home show a select to find point

--- Web/src/pages/Home/index.tsx
+++ Web/src/pages/Home/index.tsx
@@ -1,26 +1,35 @@
-import React from 'react';
+import React, { useState } from 'react';
 import logo from '../../assets/logo.svg'
 import './styles.css'
-import { FiLogIn } from 'react-icons/fi'
+import { FiLogIn, FiSearch } from 'react-icons/fi'
 import { Link } from 'react-router-dom'
+import DimmingScreenFindCity from '../../components/DimmingScreenFindCity'
 
 const Home: React.FC = () => {
+
+    const [Dimming, setDimming] = useState(false)
+    console.log(Dimming)
+
   return(
       <div id="page-home">
+          {Dimming && <DimmingScreenFindCity setOpenDimming={setDimming} />}
           <div className="content">
                 <header>
                     <img src={ logo } alt="Ecoleta"/>
+                    <Link to="/create-point">
+                        <span><FiLogIn /></span>
+                        <strong>Cadastre um ponto de coleta</strong>
+                    </Link>
                 </header>
                 <main>
                     <h1>Seu Marketplace de coleta de Resíduos.</h1>
                     <p>Ajudamos pessoas a encontrarem pontos de coleta de forma
                         eficiente.
                     </p>
-
-                    <Link to="/create-point">
-                        <span><FiLogIn /></span>
-                        <strong>Cadastre um ponto de coleta</strong>
-                    </Link>
+                    <a onClick={() => setDimming(true)}>
+                        <span><FiSearch /></span>
+                        <strong>Encontre um ponto de coleta</strong>
+                    </a>    
                 </main>
           </div>
       </div>
