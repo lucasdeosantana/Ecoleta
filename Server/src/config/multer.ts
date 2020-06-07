2534e39e82f16466e415d0b1eb8162ab470cf22c
Lucas Santana <lucasdeosantana@gmail.com>
2020-06-07 18:31:58 -0300

    now files are a slugify name before save

--- Server/src/config/multer.ts
+++ Server/src/config/multer.ts
@@ -1,12 +1,13 @@
 import multer from 'multer'
 import {UPLOAD_IMAGE_FOLDER} from './variables'
+import slugify from 'slugify'
 
 export default {
     storage: multer.diskStorage({
         destination:UPLOAD_IMAGE_FOLDER,
         filename(request, file, callback){
             const date = new Date
-            const filename = `${date.getTime()}-${file.originalname}`
+            const filename = `${date.getTime()}-${slugify(file.originalname)}`
             callback(null, filename)
         }
     })
