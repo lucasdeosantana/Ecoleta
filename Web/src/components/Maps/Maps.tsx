fd7a46cafd03d8310d2b3026203312bed49d8fcb
Lucas Santana <lucasdeosantana@gmail.com>
2020-06-07 18:42:37 -0300

    now maps'll be receive a position by city

--- Web/src/components/Maps/Maps.tsx
+++ Web/src/components/Maps/Maps.tsx
@@ -1,20 +1,32 @@
-import { Map, Marker, TileLayer } from "react-leaflet";
-import React, { useEffect, useState, useContext }  from 'react';
 import { LeafletMouseEvent } from "leaflet";
-import contextCreatePoint from '../../pages/CreatePoint/context'
+import React, { useContext, useEffect, useState } from 'react';
+import { Map, Marker, TileLayer } from "react-leaflet";
+import contextCreatePoint from '../../pages/CreatePoint/context';
+import api from '../../services/api';
+import { prospsMapsI } from "../../interfaces/appInterfaces";
 
-const Maps: React.FC = () => {
-  const context = useContext(contextCreatePoint)
+const Maps: React.FC<prospsMapsI> = ({setCity, ufCity}) => {
 
   const [initialPosition, setInitialPosition] = useState<[number, number]>([-11.3206707,-51.0105178])
   const [positionSelected, setpositionSelected] = useState<[number, number]>([0,0])
-
+  
   useEffect(()=>{
     navigator.geolocation.getCurrentPosition(position=>{
         const { latitude, longitude} = position.coords        
         setInitialPosition([latitude, longitude])
     })
   },[])
+
+  useEffect(() => {
+    if(ufCity.city===""){
+      return;
+    }
+    api.get(`uf/${ufCity.uf}/city/${ufCity.city}`).then(
+      response => {
+        setInitialPosition([response.data.latitude, response.data.longitude])
+      }
+    )
+  }, [ufCity])
 
   function handleSelectPosition(event:LeafletMouseEvent){
     const latitude  = event.latlng.lat
@@ -23,8 +35,6 @@ const Maps: React.FC = () => {
         latitude,
         longitude
     ])
-    context.latitude = latitude
-    context.longitude = longitude
 }
   return (
     <Map center={initialPosition} zoom={15} onclick={handleSelectPosition}>
