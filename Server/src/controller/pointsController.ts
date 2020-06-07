1011e55bcd3ec18c6879434f09dca5765cb7eef5
Lucas Santana <lucasdeosantana@gmail.com>
2020-06-07 18:40:38 -0300

    configured to return items id with points

--- Server/src/controller/pointsController.ts
+++ Server/src/controller/pointsController.ts
@@ -1,4 +1,3 @@
-import { Map } from 'react-leaflet';
 import {Request, Response} from 'express'
 import knex from '../database/connection'
 import { BASE_URL, DEBUG, UPLOAD_IMAGE_URL_PATH } from '../config/variables'
@@ -17,7 +16,15 @@ class PointsController{
         .distinct()
         .select('points.*') 
         DEBUG && console.log(`Receive request in route: ${request.url}, and response ${points.length} points`)
-        response.json(points)
+        const serializedPoints = points.map(async point=>{
+            const itemsId = await knex('point_items').select('item_id').where({point_id:point.id})
+            const items= itemsId.map(resp => resp.item_id)
+            return {...point, imageUrl:`${BASE_URL}${UPLOAD_IMAGE_URL_PATH}${point.image}`, items}
+        })
+        Promise.all(serializedPoints).then(
+            finish=>response.json(finish)
+        )  
+        
     }
     async create(request:Request, response:Response){
         DEBUG && console.log(`Receive request in route: ${request.url}, and redirect to PointsController>create`)
@@ -25,13 +32,13 @@ class PointsController{
             longitude, city,  uf,   items } = request.body
             const trx = await knex.transaction()
             const point_ids = await trx('points').insert({
-                name:String(name).toLowerCase(),  
-                email:String(email).toLowerCase(),
-                whatsapp:String(whatsapp).toLowerCase(),
-                latitude:String(latitude).toLowerCase(),
-                longitude:String(longitude).toLowerCase(),
-                city:String(city).toLowerCase(),
-                uf:String(uf).toLowerCase(), 
+                name:name,  
+                email:email,
+                whatsapp:whatsapp,
+                latitude:Number(latitude),
+                longitude:Number(longitude),
+                city:city,
+                uf:uf, 
                 image:request.file.filename})
                 
                 const point_id = point_ids[0]
@@ -45,7 +52,7 @@ class PointsController{
                 })
                 console.log(pointItems)
                 await trx('point_items').insert(pointItems)
-                await trx.commit()
+                trx.commit()
                 return response.json({success:true, id:point_id})
             }
     async show(request:Request, response:Response){
@@ -62,7 +69,7 @@ class PointsController{
         .join('point_items', 'items.id', '=', 'point_items.item_id')
         .where('point_items.point_id', id).select('items.id').select('items.title')
         const pointWithUrl={...point, imageUrl:`${BASE_URL}${UPLOAD_IMAGE_URL_PATH}${point.image}`}
-        return response.json({pointWithUrl, items})
+        return response.json({point:pointWithUrl, items})
     }
 }
         
