92b9ab0d63fc62946ee82f18053e3724f8a3e6bc
Lucas Santana <lucasdeosantana@gmail.com>
2020-06-07 18:36:26 -0300

    create component Dimming to find point

---
+++ Web/src/components/DimmingScreenFindCity/DimmingScreenFindCity.tsx
@@ -0,0 +1,23 @@
+import React, { useContext, useState } from 'react';
+import './styles.css';
+import { Link } from 'react-router-dom'
+import UfCity from '../UfCity'
+
+import { FiX  } from 'react-icons/fi'
+import { propsDimmingScreenFindCityI, ufCityI } from '../../interfaces/appInterfaces'
+
+
+const DimmingScreenFindCity: React.FC<propsDimmingScreenFindCityI> = ({ setOpenDimming }) => {
+  const [city, setcity] = useState<ufCityI>({uf:'', city:''})
+  return (
+        <div className="dimming">
+          <button id="close" onClick={()=>setOpenDimming(false)}><FiX /></button>
+          <div className="container">
+            <UfCity ufCity={city} setCity={setcity} />
+            <Link to={{pathname: "/find-point/", search:`?uf=${city.uf}&city=${city.city}`}}>Buscar</Link>
+          </div>
+        </div>
+  );
+}
+
+export default DimmingScreenFindCity;
---
+++ Web/src/components/DimmingScreenFindCity/index.tsx
@@ -0,0 +1,3 @@
+import DimmingScreenFindCity from './DimmingScreenFindCity';
+
+export default DimmingScreenFindCity
