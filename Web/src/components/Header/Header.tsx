8692a75b3883cb9ae0ca09305400eef17b76cd21
Lucas Santana <lucasdeosantana@gmail.com>
2020-06-07 18:41:53 -0300

    add styles and link in logo

--- Web/src/components/Header/Header.tsx
+++ Web/src/components/Header/Header.tsx
@@ -2,13 +2,13 @@ import React  from 'react';
 import { Link } from "react-router-dom";
 import { FiArrowLeft } from "react-icons/fi";
 import logo  from '../../assets/logo.svg'
-
+import './style.css'
 
 const Header: React.FC = (props) => {
 
   return (
       <header>
-          <img src={ logo } alt="Ecoleta"/>
+          <Link to="/"><img src={ logo } alt="Ecoleta"/></Link>
           <Link to="/">
               <FiArrowLeft />
               Voltar para a Home
